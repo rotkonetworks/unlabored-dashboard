b7f4b23f05486dd33c43d5a243ff301bc14c9372
hitchhooker <tommi@niemi.lol>
2023-08-19 07:43:03 +0700

    fix humanreadable

--- frontend/src/components/ContainerInfo.tsx
+++ frontend/src/components/ContainerInfo.tsx
@@ -35,7 +35,7 @@ export default function ContainerInfo(props: ContainerProps): JSX.Element {
 function humanReadableSize(bytes: number): string {
     const sizes = ['Bit/s', 'KBit/s', 'MBit/s', 'GBit/s', 'TBit/s'];
     if (bytes === 0) return '0 Bit/s';
-    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString(), 10);
-    if (i == 0) return bytes + ' ' + sizes[i]; // For Bit/s, no decimals needed
+    const i = Math.floor(Math.log(bytes) / Math.log(1024));
+    if (i === 0) return Math.round(bytes) + ' ' + sizes[i];
     return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i];
 }
