dfcee98ddeca7e71d345d28e2456f304315f7489
hitchhooker <tommi@niemi.lol>
2023-08-24 02:53:12 +0700

    rm fdqn and use just name

--- frontend/src/components/ContainerInfo.tsx
+++ frontend/src/components/ContainerInfo.tsx
@@ -36,7 +36,7 @@ export default function ContainerInfo(props: ContainerProps): JSX.Element {
 
   return (
     <div class="font-mono p-4 border my-4 filter-drop-shadow bg-hex-DFE9C5 rounded shadow-sm text-hex-010001">
-      <h3 class="text-xl text-center">{props.container.hostname}</h3>
+      <h3 class="text-xl text-center">{props.container.hostname.split('.')[0]}</h3>
       {network && role && <p>{`${network} ${role} ${instance}`}</p>}
       <p>Status: <StatusBar status={props.container.status} /></p>
       <p>CPU Usage: <UsageBar current={props.container.cpu} max={1} /></p>
