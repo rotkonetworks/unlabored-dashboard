a99b72475e2e1e54105cfbe17c8e38dd02b997e0
hitchhooker <tommi@niemi.lol>
2023-08-27 04:03:35 +0700

    add extended naming logic

--- frontend/src/components/ContainerInfo.tsx
+++ frontend/src/components/ContainerInfo.tsx
@@ -20,19 +20,53 @@ interface ContainerProps {
 export default function ContainerInfo(props: ContainerProps): JSX.Element {
   const statusClass = props.container.status === 'running' ? 'bg-hex-AECE4B' : 'bg-red'; // Neon green for running status
 
+  // Extract the relevant digits from container.id
+  const firstDigit = String(container.id)[0];
+  const secondDigit = String(container.id)[1];
+  const thirdDigit = String(container.id)[2];
+
+  // Role mapping based on the first digit
   const role = {
     '1': 'Validator',
     '2': 'Bootnode',
-    '3': 'Endpoint'
-  }[String(container.id)[0]];
+    '3': 'RPC Endpoint',
+    '4': 'Collator',
+    '5': 'Bootnode',
+    '6': 'RPC Endpoint'
+  }[firstDigit];
 
-  const network = {
+  // Network mapping based on the second digit for primary networks
+  const primaryNetworks = {
     '1': 'Polkadot',
     '2': 'Kusama',
     '3': 'Westend'
-  }[String(container.id)[1]];
+  };
 
-  const instance = String(container.id)[2].padStart(2, '0');
+  // Network mapping for parachains
+  const parachains = {
+    '0': 'Encointer',
+    '1': 'Statemint',
+    '2': 'Statemine',
+    '3': 'Westmint',
+    '4': 'Polkadot Bridge Hub',
+    '5': 'Kusama Bridge Hub',
+    '6': 'Westend Bridge Hub',
+    '7': 'Polkadot Collectives Hub',
+    '8': 'Kusama Collectives Hub',
+    '9': 'Westend Collectives Hub'
+  };
+
+  // Determine which network mapping to use based on the first digit
+  let network;
+  if (['1', '2', '3'].includes(firstDigit)) {
+    network = primaryNetworks[secondDigit];
+  } else if (['4', '5', '6'].includes(firstDigit)) {
+    network = parachains[secondDigit];
+  }
+
+  // Instance based on the third digit
+  const instance = thirdDigit.padStart(2, '0');
+
 
   return (
     <div class="font-mono p-4 border my-4 filter-drop-shadow bg-hex-DFE9C5 rounded shadow-sm text-hex-010001">
